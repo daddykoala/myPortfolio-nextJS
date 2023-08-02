ca63ab13130df46da31f388c0506a31e7095b26f
Clément Moretti <98534268+daddykoala@users.noreply.github.com>
2023-08-02 17:07:48 +0200

    styles:fix h1

--- portfolio-next/component/header/header.js
+++ portfolio-next/component/header/header.js
@@ -1,22 +1,44 @@
-import React from 'react'
-import styles from '@/styles/header.module.scss'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/styles/header.module.scss";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-        <h1 className={styles.header__item} >
-            Moretti Clément</h1>
-            <svg  className={styles.header__cornereffect}xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#F9F8F6"></path></svg>
-        <div className={styles.header__item}>
-            <navbar className={styles.navbar}>
-                <nav className={styles.navbar__items}>Contact</nav>
-                <nav className={styles.navbar__items}>Projets</nav>
-                <nav className={styles.navbar__items}>A propos</nav>
-                
-            </navbar>
-        </div>
-        <div className={styles.header__item}>s</div>
+      <h1 className={styles.header__item}>Moretti Clément</h1>
+      <svg
+        className={styles.header__cornereffect}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 100 100"
+      >
+        <path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#ffffff"></path>
+      </svg>
+      <div className={styles.header__item}>
+        <navbar className={styles.navbar}>
+          <nav className={styles.navbar__items}>Contact</nav>
+          <nav className={styles.navbar__items}>Projets</nav>
+          <nav className={styles.navbar__items}>A propos</nav>
+          <nav className={styles.navbar__items}>Projets</nav>
+          <nav className={styles.navbar__items}>
+            <Image
+              src="/png/linkedin.png"
+              width={30}
+              height={30}
+              alt="logo linkedin"
+            />
+          </nav>
+          <nav className={styles.navbar__items}>
+            <Image
+              src="/png/wattsapp.png"
+              width={30}
+              height={30}
+              alt="logo linkedin"
+            />
+          </nav>
+        </navbar>
+      </div>
+      <div className={styles.header__item}>s</div>
     </header>
-    
-  )
+  );
 }
