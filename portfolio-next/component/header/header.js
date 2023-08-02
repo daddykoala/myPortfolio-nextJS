0e1543af205e1fd4bf847e1170bb885fabd823c9
Clément Moretti <98534268+daddykoala@users.noreply.github.com>
2023-08-02 14:28:38 +0200

    nettoyage présentation

---
+++ portfolio-next/component/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '@/styles/header.module.css'
+
+export default function header() {
+  return (
+    <header className="header">
+        <div>s</div>
+        <div>s</div>
+        <div>s</div>
+    </header>
+    
+  )
+}
