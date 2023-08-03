3a94f75f6ecc37687e0a4761f1d84708537d34ce
Clément Moretti <98534268+daddykoala@users.noreply.github.com>
2023-08-03 13:48:01 +0200

    🔧 config: ajout etat button menu

--- portfolio-next/component/header/header.js
+++ portfolio-next/component/header/header.js
@@ -3,14 +3,23 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "@/styles/header.module.scss";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 export default function Header() {
+  //je recupere la valeur de l'ecran
   const screenWidth = useSelector((state) => state.screen.screenWidth);
   console.log(screenWidth);
   //composant image reutilisable car toute les images de la meme taille.
   const MyImageComponent = ({ src, alt }) => (
     <Image src={src} alt={alt} width={30} height={30} />
   );
+
+  //je crée une variable pour l'état du menu fals car fermé a l'uverture de l'app sur mobile
+const [openMenu, setOpenMenu] =useState(false);
+const handleOpenMenu = (e) => {
+setOpenMenu(!openMenu)
+}
+
 
   return (
     <header className={styles.header}>
@@ -44,7 +53,8 @@ export default function Header() {
         </>
       ) : (
         <div className={styles.header__1024px} >
-          <h2 className={`${styles.header__button}`}>Menu </h2>
+          { !openMenu ? <h2 className={`${styles.header__button}`} onClick={handleOpenMenu}>Menu </h2> :<h2 className={`${styles.header__button}`} onClick={handleOpenMenu}>Close </h2>  }
+          
           <ul className={styles.header__socials}>
           <li className={styles.navbar__items}>
 
@@ -56,23 +66,8 @@ export default function Header() {
 
           </ul>
         </div>
-        // <div className={styles.header__item}>
-        //   <ul className={styles.navbar}>
-        //     <nav className={styles.navbar__items}>
-        //       <MyImageComponent src="/png/wattsapp.png" alt="logo linkedin" />
-        //     </nav>
-        //     <nav className={styles.navbar__items}>
-        //       <MyImageComponent src="/png/wattsapp.png" alt="logo linkedin" />
-        //     </nav>
-        //     <nav className={styles.navbar__items}>
-        //       <MyImageComponent src="/png/wattsapp.png" alt="logo linkedin" />
-        //     </nav>
-        //     <nav className={styles.navbar__items}>
-        //       <MyImageComponent src="/png/wattsapp.png" alt="logo linkedin" />
-        //     </nav>
-        //   </ul>
-        //     
-        // </div>
+        
+        // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#F9F8F6"></path></svg>
       )}
     </header>
   );
