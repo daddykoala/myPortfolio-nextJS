0e4a8b5c4f33dac2d52455864f1d844835a3ba36
Clément Moretti <98534268+daddykoala@users.noreply.github.com>
2023-08-04 18:25:51 +0200

    creation logodata

--- portfolio-next/component/about/about.js
+++ portfolio-next/component/about/about.js
@@ -3,12 +3,26 @@ import { useSelector } from "react-redux";
 import styles from "@/styles/about.module.scss";
 import Image from "next/image";
 import Link from "next/link";
+//IMAGE PARALLAX
+import logoData from "../../data/logoData";
 import ME from "../../public/png/imageprofil.png";
-import INSTA from "../../public/png/instagram.png";
+import INSTA from "../../public/png/halftone.png";
+import REACT from "../../public/png/logo_react.png";
+import DOCKER from "../../public/png/logo_docker.png";
+import JS from "../../public/png/logo_js.png";
+import NEXT from "../../public/png/logo_next.png";
+import NODES from "../../public/png/logo_nodes.png";
+import POSTGRES from "../../public/png/logo_postgres.png";
 function About() {
   //leger parallax lorsqu'on scroole sur page je modifie selon la couche la vitesse de defilement.
   const screenWidth = useSelector((state) => state.screen.screenWidth);
   console.log(screenWidth);
+
+  //je crée un objet image  standard pour mes logos 
+
+//je crée un objet datalogo pour mieux mapper et la mainetance du code 
+
+
 
   useEffect(() => {
     const handleScroll = () => {
@@ -19,8 +33,12 @@ function About() {
       const image2 = document.querySelector(
         "." + styles.about__profil__parallax2
       );
+      const image3 = document.querySelector(
+        "." + styles.about__profil__parallax3
+      );
       image1.style.transform = "translateY(" + -scrollPosition * 0.2 + "px)";
       image2.style.transform = "translateY(" + -scrollPosition * 0.1 + "px)";
+      image3.style.transform = "translateY(" + scrollPosition * 0.1 + "px)";
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -49,6 +67,18 @@ function About() {
               <span>Contact</span>
             </Link>
           </div>
+        </div>
+        <div>
+        {/* {logoData.map((logo, index) => (
+        <Image
+          key={index}
+          className={styles[logo.className]}
+          src={logo.src}
+          alt={logo.alt}
+          width={screenWidth > 1024 ? 90 : 60}
+          height={screenWidth > 1024 ? 90 : 60}
+        />
+      ))} */}
         </div>
         <div className={styles.about__motivation}>
           <svg
@@ -87,6 +117,7 @@ function About() {
           width={screenWidth > 1024 ? 450 : 350}
           height={screenWidth > 1024 ? 450 : 350}
         />
+        
       </div>
     </section>
   );
---
+++ portfolio-next/data/logoData.js
@@ -0,0 +1,34 @@
+const logoData = [
+  {
+    src: "/png/logo_docker.png",
+    alt: "Docker logo",
+    className: "about__profil__parallax3",
+  },
+  {
+    src: "/png/logo_js.png",
+    alt: "JavaScript logo",
+    className: "about__profil__parallax3",
+  },
+  {
+    src: "/png/logo_nodes.png",
+    alt: "Node.js logo",
+    className: "about__profil__parallax3",
+  },
+  {
+    src: "/png/logo_postgres.png",
+    alt: "Postgres logo",
+    className: "about__profil__parallax3",
+  },
+  {
+    src: "/png/logo_next.png",
+    alt: "Next.js logo",
+    className: "about__profil__parallax3",
+  },
+  {
+    src: "/png/logo_react.png",
+    alt: "Ract.js logo",
+    className: "about__profil__parallax3",
+  },
+];
+
+export default logoData;
