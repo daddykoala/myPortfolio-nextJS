f69f62c02db168b7fe8260bd4db516b4509d0f7c
Clément Moretti <98534268+daddykoala@users.noreply.github.com>
2023-08-08 15:55:50 +0200

    modif:projet ajout d'un bouton+style

--- portfolio-next/component/projet/projet.js
+++ portfolio-next/component/projet/projet.js
@@ -3,6 +3,7 @@ import styles from "@/styles/projet.module.scss";
 import dataProjet from "../../data/dataProjet";
 import devicon from "../../data/devicon";
 import Image from "next/image";
+import Link from "next/link";
 
 function projet() {
   return (
@@ -62,6 +63,26 @@ function projet() {
                 
               );
             })}
+            <div className={styles.projet__container__link}>
+
+            <svg
+            className={styles.projet__container__link__corner1}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 100"
+          >
+            <path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#ffffff"></path>
+          </svg>
+          <svg
+            className={styles.projet__container__link__corner2}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 100"
+          >
+            <path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#ffffff"></path>
+          </svg>
+            <Link href={projet.link_web}  className={styles.projet__container__link__button} >Visit
+            </Link>
+
+            </div>
           </article>
         );
       })}
