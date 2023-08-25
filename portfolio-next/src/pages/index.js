a7c851ea4a02f2128c85541dad7e4fb5068a1da5
Clément Moretti <98534268+daddykoala@users.noreply.github.com>
2023-08-25 18:13:26 +0200

    fix: layer parallax about

--- portfolio-next/src/pages/index.js
+++ portfolio-next/src/pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
         <Header/>
         <div className={`${styles.home} `}>
         <About/>
-        <Info/>
+        {/* <Info/> */}
         </div>
           <Projet/>
           <Contact/>
