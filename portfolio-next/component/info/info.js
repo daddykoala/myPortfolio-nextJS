59d2d19a81ad328781d6f6a9f7923f6eb4276f61
Clément Moretti <98534268+daddykoala@users.noreply.github.com>
2023-08-25 17:51:56 +0200

    modif : responsive footer

--- portfolio-next/component/info/info.js
+++ portfolio-next/component/info/info.js
@@ -4,9 +4,10 @@ import styles from "@/styles/info.module.scss";
 export default function info() {
   return (
     <section className={styles.info}>
-        <div className={styles.info__story}> animation framer motion</div>
+      <div className={styles.info__features}>En cours de réalisations </div>
+        {/* <div className={styles.info__story}> animation framer motion</div>
         <div className={styles.info__statistique}> stats github</div>
-        <div className={styles.info__projet}> swiper link project</div>
+        <div className={styles.info__projet}> swiper link project</div> */}
     </section>
   )
 }
